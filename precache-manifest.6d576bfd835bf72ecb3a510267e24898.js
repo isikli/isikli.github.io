2ad3d628205a1b2e83795144ab5579a40df55af0
isikli <isiklia@gmail.com>
2020-04-19 16:23:12 +1000

    .

---
+++ precache-manifest.6d576bfd835bf72ecb3a510267e24898.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a1588e611a714ed87f9b167790bda1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b9a9594c914c25b7ef8",
+    "url": "/static/css/main.45f08c2b.chunk.css"
+  },
+  {
+    "revision": "2abb84a6e605b8603e4d",
+    "url": "/static/js/2.6551f0ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6551f0ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b9a9594c914c25b7ef8",
+    "url": "/static/js/main.4ad1a2a4.chunk.js"
+  },
+  {
+    "revision": "1fdd19870f54d24a478e",
+    "url": "/static/js/runtime-main.e6c8655e.js"
+  }
+]);
